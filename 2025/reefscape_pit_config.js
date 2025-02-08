9c465cfc39b4001579266b9b53ef0ce910a18005
TheRealGreydon <teslasbiggestfan@gmail.com>
2025-02-08 14:18:56 -0500

    Copyed over config

--- 2025/reefscape_pit_config.js
+++ 2025/reefscape_pit_config.js
@@ -4,34 +4,90 @@ var config_data = `
     "title": "Scouting PASS 2025",
     "page_title": "Reefscape",
     "pitConfig": "true",
-    "prematch": [
+    "prematch": [   
         { 
             "name": "Team Number",
-            "code": "t",
+            "code": "sc",
             "type": "number"
         },
         { 
-            "name": "Width",
+            "name": "Width(Ft)",
+            "code": "wid",
+            "type": "number",
+            "defaultValue": "0"
+        },
+        {
+            "name": "Can score on - L1",
+            "code":"ch",
+            "type":"checkbox",
+            "choices":
+            {
+                "L1": "L1"
+            }
+        },
+        {
+            "name": "L2",
+            "code":"ch",
+            "type":"checkbox",
+            "choices":
+            {
+                "L2": "L2"
+            }
+        },
+        {
+            "name": "L3",
+            "code":"ch",
+            "type":"checkbox",
+            "choices":
+            {
+                "L3": "L3"
+            }
+        },
+        {
+            "name": "L4",
+            "code":"ch",
+            "type":"checkbox",
+            "choices":
+            {
+                "L4": "L4"
+            }
+        },
+        {
+            "name": "Processor",
+            "code":"ch",
+            "type":"checkbox",
+            "choices":
+            {
+                "Processor": "Processor"
+            }
+        },
+        {
+            "name": "Net",
+            "code":"ch",
+            "type":"checkbox",
+            "choices":
+            {
+                "Net": "Net"
+            }
+        },
+        { 
+            "name": "How fast can your robot move? (MPH)",
             "code": "wid",
             "type": "number",
             "defaultValue": "0"
         },
         { 
-            "name": "Floor Pickup Coral",
+            "name": "Can climb on -",
             "code": "fpu",
-            "type": "bool"
-        },
-        { 
-            "name": "Autos",
-            "code": "aut",
-            "type": "bool"
-        },
-        {
-            "name": "Can you create new Auto's between matches",
-            "code": "nwa",
-            "type": "bool"
+            "type": "radio",
+            "choices": 
+            {
+                "Shallow": "Shallow Cage",
+                "Deep": "Deep Cage"
+            }
         },
         {
+            "Name": "Can you remove algae from the reef",
             "name": "Can you remove algae from the reef",
             "code": "rva",
             "type": "bool"
