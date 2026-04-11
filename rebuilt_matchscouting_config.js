786dc1b32458229f4f62c374b719a4ca5d3a7be7
John Hurrell <jhurrell@gmail.com>
2026-04-11 11:09:46 -0400

    Changed the event key.

--- rebuilt_matchscouting_config.js
+++ rebuilt_matchscouting_config.js
@@ -9,7 +9,7 @@ var config_data = `
             "name": "Event",
             "code": "e",
             "type": "event",
-            "defaultValue": "2026ohmv",
+            "defaultValue": "2026nytr",
             "required": "true"
         },
         {
